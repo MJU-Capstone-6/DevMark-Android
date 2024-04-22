dbf9da94710a5277590972d7e3d91f085acf8561
HamBeomJoon <hbj0802@naver.com>
2024-04-22 12:33:28 +0900

    [MARK-016] 네비게이션 바 생성, 연결

--- DevMark/app/src/main/java/com/devmark/devmark/MainActivity.kt
+++ DevMark/app/src/main/java/com/devmark/devmark/MainActivity.kt
@@ -3,14 +3,36 @@ package com.devmark.devmark
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.devmark.devmark.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val bn: BottomNavigationView by lazy {
+        binding.bottomNav
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // binding 세팅
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // 첫 화면 workspace뜨게 하기
+        bn.selectedItemId = R.id.menu_workspace
+        supportFragmentManager.beginTransaction().add(binding.frameLayout.id, WorkspaceFragment()).commit()
+        bn.setOnItemSelectedListener { item ->
+            replaceFragment(
+                when (item.itemId) {
+                    R.id.menu_recommend -> RecommendFragment()
+                    R.id.menu_remind -> RemindFragment()
+                    else -> WorkspaceFragment()
+                }
+            )
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, fragment).commit()
     }
 }
