deae51aae28b78048428ec8337fd21b4e4b673b7
MunJangHun <105299421+moondev03@users.noreply.github.com>
2024-04-30 18:36:15 +0900

    [MARK-082] Retrofit Client & HttpLoggingInterceptor 추가 (#15)
    * [MARK-082] Implementation: Retrofit / Gson / LoggingInterceptor
    
    * [MARK-082] Retrofit Client 생성 및 LoggingInterceptor 추가

---
+++ DevMark/app/src/main/java/com/devmark/devmark/data/remote/RetrofitClient.kt
@@ -0,0 +1,36 @@
+package com.devmark.devmark.data.remote
+
+import com.devmark.devmark.BuildConfig
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitClient {
+    private var instance: Retrofit? = null
+    private val gson = GsonBuilder().setLenient().create()
+
+    private val loggingInterceptor = if(BuildConfig.DEBUG){
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }else {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    fun getInstance(): Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(BuildConfig.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build()
+        }
+
+        return instance!!
+    }
+}
