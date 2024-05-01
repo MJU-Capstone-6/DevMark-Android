f5b589f462e6623d0f3490060a6d741de961b1ca
HamBeomJoon <hbj0802@naver.com>
2024-05-01 15:47:14 +0900

    [MARK-020] 워크스페이스 관리 화면

---
+++ DevMark/app/src/main/java/com/devmark/devmark/presentation/WorkspaceSetting/MemberInfoLvAdapter.kt
@@ -0,0 +1,43 @@
+package com.devmark.devmark.presentation.WorkspaceSetting
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.BaseAdapter
+import android.widget.ListView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.devmark.devmark.R
+import com.devmark.devmark.data.Member
+import com.devmark.devmark.databinding.ItemMemberInfoBinding
+
+class MemberInfoLvAdapter(val context: Context, private val memberList: List<Member>) :
+    BaseAdapter() {
+    private var mBinding: ItemMemberInfoBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        mBinding = ItemMemberInfoBinding.inflate(LayoutInflater.from(context))
+
+        val params = ConstraintLayout.LayoutParams(MATCH_PARENT, (32 * binding.clItemMember.resources.displayMetrics.density).toInt())
+        binding.clItemMember.layoutParams = params
+
+        binding.tvNameBookmarkInfo.text = memberList[position].name
+        val formattedText = context.getString(R.string.num_bookmark_info, memberList[position].bookmarkNum.toString())
+        binding.tvNumBookmarkInfo.text = formattedText
+        return mBinding!!.root
+    }
+
+    override fun getCount(): Int = memberList.size
+
+    override fun getItem(position: Int): Any {
+        return memberList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+}
