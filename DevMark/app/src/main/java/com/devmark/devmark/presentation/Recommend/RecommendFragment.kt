1e924541ccce537c7563d1830cdb6066dfa21a00
HamBeomJoon <hbj0802@naver.com>
2024-05-06 17:28:00 +0900

    [MARK-025] 추천 포스트 adapter, 화면 설정

--- DevMark/app/src/main/java/com/devmark/devmark/presentation/Recommend/RecommendFragment.kt
+++ DevMark/app/src/main/java/com/devmark/devmark/presentation/Recommend/RecommendFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.devmark.devmark.MainActivity
+import com.devmark.devmark.data.BookMark
+import com.devmark.devmark.data.TopCategory
 import com.devmark.devmark.databinding.FragmentRecommendBinding
-import com.devmark.devmark.databinding.FragmentWorkspaceBinding
 
 class RecommendFragment : Fragment() {
+    private val recommendList = ArrayList<BookMark>()
+    private val topCategoryList = ArrayList<TopCategory>()
     private lateinit var binding: FragmentRecommendBinding
+    private lateinit var recommendAdapter: RecommendRvAdapter
+    private lateinit var topCategoryAdapter: TopCategoryLvAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +26,40 @@ class RecommendFragment : Fragment() {
         (requireActivity() as MainActivity).changeNaviVisibility(true)
         binding = FragmentRecommendBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        topCategoryList.add(TopCategory("Android", 22))
+        topCategoryList.add(TopCategory("Kotlin", 10))
+        topCategoryList.add(TopCategory("Golang", 6))
+        topCategoryAdapter = TopCategoryLvAdapter(requireContext(), topCategoryList)
+        binding.lvTopCategory.apply {
+            adapter = topCategoryAdapter
+        }
+
+        // 데이터 추가
+        recommendList.add(
+            BookMark(
+                "[Android] 깔쌈하게 MVVM 패턴과 AAC 알아보기",
+                "Android"
+            )
+        )
+        recommendList.add(BookMark("메타는 왜 안드로이드 앱을 코틀린으로 다시 짜나", "Android"))
+        recommendList.add(BookMark("우아한테크세미나 코프링 정리", "Kotlin"))
+
+        // recyclerview adapter
+        recommendAdapter = RecommendRvAdapter()
+
+        // set up recyclerview
+        binding.rvRecommend.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = recommendAdapter
+        }
+
+        // set data
+        recommendAdapter.setData(recommendList)
     }
 }
