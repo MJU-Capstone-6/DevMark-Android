b80ca571558ca360bed35a56df00ede487d2722f
HamBeomJoon <hbj0802@naver.com>
2024-05-18 03:06:03 +0900

    [MARK-107] WorkspaceInfo API

--- DevMark/app/src/main/java/com/devmark/devmark/data/remote/api/WorkSpaceService.kt
+++ DevMark/app/src/main/java/com/devmark/devmark/data/remote/api/WorkSpaceService.kt
@@ -2,11 +2,14 @@ package com.devmark.devmark.data.remote.api
 
 import com.devmark.devmark.data.model.workspace.RequestWorkSpaceCreateDTO
 import com.devmark.devmark.data.model.workspace.ResponseWorkSpaceCreateDTO
+import com.devmark.devmark.data.model.workspace.ResponseWorkSpaceInfoDTO
 import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface WorkSpaceService {
     @POST("/api/v1/workspace")
@@ -14,4 +17,10 @@ interface WorkSpaceService {
         @Header("Authorization") accessToken: String,
         @Body body: RequestWorkSpaceCreateDTO
     ): Response<ResponseWorkSpaceCreateDTO>
+
+    @GET("/api/v1/workspace/{id}")
+    suspend fun getWorkspaceInfo(
+        @Header("Authorization") accessToken: String,
+        @Path("id") workspaceId: Int,
+    ): Response<ResponseWorkSpaceInfoDTO>
 }
--- DevMark/app/src/main/java/com/devmark/devmark/domain/repository/WorkSpaceRepository.kt
+++ DevMark/app/src/main/java/com/devmark/devmark/domain/repository/WorkSpaceRepository.kt
@@ -2,10 +2,16 @@ package com.devmark.devmark.domain.repository
 
 import com.devmark.devmark.domain.model.user.WorkspaceEntity
 import com.devmark.devmark.domain.model.workspace.RequestWorkSpaceCreateEntity
+import com.devmark.devmark.domain.model.workspace.WorkSpaceInfoEntity
 
 interface WorkSpaceRepository {
     suspend fun createWorkspace(
         accessToken: String,
         body: RequestWorkSpaceCreateEntity
     ): Result<WorkspaceEntity>
+
+    suspend fun getWorkspaceInfo(
+        accessToken: String,
+        workspaceId: Int
+    ): Result<WorkSpaceInfoEntity>
 }
