ffd54be271fe3b6820accf5bb77d60cc98c51060
HamBeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2024-05-19 17:49:55 +0900

    [MARK-111] 북마크 목록 조회 (API) (#46)
    * [MARK-111] current workspace 중복호출 수정
    
    * [MARK-111] 북마크 API 연동
    
    * [MARK-111] 멤버, 카테고리 필터 북마크 호출

---
+++ DevMark/app/src/main/java/com/devmark/devmark/data/mapper/bookmark/BookmarksMapper.kt
@@ -0,0 +1,18 @@
+package com.devmark.devmark.data.mapper.bookmark
+
+import com.devmark.devmark.data.model.bookmark.ResponseBookmarksDTO
+import com.devmark.devmark.domain.model.bookmark.BookmarksEntity
+
+object BookmarksMapper {
+    fun mapperToResponseEntity(item: List<ResponseBookmarksDTO>): List<BookmarksEntity> {
+        return item.map {
+            BookmarksEntity(
+                categoryId = it.categoryId, id = it.id, link = it.link,
+                summary = it.summary,
+                title = it.title,
+                userId = it.userId,
+                workspaceId = it.workspaceId
+            )
+        }
+    }
+}
---
+++ DevMark/app/src/main/java/com/devmark/devmark/data/model/bookmark/ResponseBookmarksDTO.kt
@@ -0,0 +1,25 @@
+package com.devmark.devmark.data.model.bookmark
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseBookmarksDTO(
+    @SerializedName("category_id")
+    val categoryId: Int,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("summary")
+    val summary: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("updated_at")
+    val updatedAt: String,
+    @SerializedName("user_id")
+    val userId: Int,
+    @SerializedName("workspace_id")
+    val workspaceId: Int
+)
