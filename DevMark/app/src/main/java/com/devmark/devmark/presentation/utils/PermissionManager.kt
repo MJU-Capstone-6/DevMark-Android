aba4267073a1b69e4c41349d2ffda5d7b55e374f
moondev03 <moondev03@gmail.com>
2024-05-29 12:33:56 +0900

    [MARK-116] FCM 세팅

---
+++ DevMark/app/src/main/java/com/devmark/devmark/presentation/utils/PermissionManager.kt
@@ -0,0 +1,28 @@
+package com.devmark.devmark.presentation.utils
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.widget.Toast
+
+object PermissionManager {
+    const val PERMISSION_REQUEST_CODE = 5000
+
+    fun checkAndRequestNotificationPermission(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val permissionCheck = ContextCompat.checkSelfPermission(
+                activity,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            )
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+}
