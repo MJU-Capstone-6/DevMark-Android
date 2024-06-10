0dfe7fdaa9b0e41d45a53eeed4b85d2a41ccd8cf
HamBeomJoon <hbj0802@naver.com>
2024-06-10 14:52:59 +0900

    [chore] 더미 데이터 클래스 제거

--- DevMark/app/src/main/java/com/devmark/devmark/domain/model/Member.kt
+++
@@ -1,6 +0,0 @@
-package com.devmark.devmark.domain.model
-
-data class Member(
-    val name: String,
-    val bookmarkNum: Int
-)
--- DevMark/app/src/main/java/com/devmark/devmark/domain/model/Remind.kt
+++
@@ -1,7 +0,0 @@
-package com.devmark.devmark.domain.model
-
-data class Remind(
-    val title: String,
-    val category: String,
-    val time: String
-)
